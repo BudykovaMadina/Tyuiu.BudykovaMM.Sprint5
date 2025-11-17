83a96d3e4e525c9e696ccfb25c913c2e0222f345
Мадина <mbudykova@mail.ru>
2025-11-17 21:37:22 +0500

    Task2

---
+++ Tyuiu.BudykovaMM.Sprint5.Task2.V24.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Tyuiu.BudykovaMM.Sprint5.Task2.V24.Lib;
+namespace Tyuiu.BudykovaMM.Sprint5.Task2.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekcedExistsFile()
+        {
+            DataService ds = new DataService();
+            int[,] mtrx = new int[3, 3];
+            string path = ds.SaveToFileTextData(mtrx);
+            FileInfo fl = new FileInfo(path);
+            bool fileExists = fl.Exists;
+
+            Assert.IsTrue(fileExists);
+        }
+    }
+}
---
+++ Tyuiu.BudykovaMM.Sprint5.Task2.V24/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.BudykovaMM.Sprint5.Task2.V24.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] mtrx = new int[3, 3] { { 6, 0, 6 },
+                                      {-1, -8, 7 },
+                                      { 5, 1, 0 } };
+
+        int rows = mtrx.GetUpperBound(0) + 1;
+        int columns = mtrx.Length / rows;
+
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 │ Выполнила: Будыкова М. М. │ ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл.     *");
+        Console.WriteLine("* Вариант #24                                                             *");
+        Console.WriteLine("* Выполнила: Будыкова Мадина Маратовна │ ПИНб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string res = ds.SaveToFileTextData(mtrx);
+
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
